bb3ebc055fd6965d5a5109f6849318859da18f89
Rodrigo Paixao de Souza <rodrigo.p.souza@aln.senaicimatec.edu.br>
2022-08-27 15:38:22 -0300

    Tela de cadastro

--- Dashboard/src/app/login/login.module.ts
+++ Dashboard/src/app/login/login.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,19 +6,21 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormLoginComponent } from './form-login/form-login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     FormLoginComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
     LoginRoutingModule,
     FormsModule,
-    MensagemModule
+    MensagemModule,
+    ReactiveFormsModule
   ],
   exports: [
     LoginComponent
